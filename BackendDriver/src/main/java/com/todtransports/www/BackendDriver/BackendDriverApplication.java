3ceaa36fb4025f8f74608ccada1fb438ad236383
Ishaan Godara <gsurendra724@gmail.com>
2018-12-15 10:20:01 +0530

    1
    1

---
+++ BackendDriver/src/main/java/com/todtransports/www/BackendDriver/BackendDriverApplication.java
@@ -0,0 +1,14 @@
+package com.todtransports.www.BackendDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDriverApplication.class, args);
+	}
+
+}
+
---
+++ BackendDriver/src/test/java/com/todtransports/www/BackendDriver/BackendDriverApplicationTests.java
@@ -0,0 +1,17 @@
+package com.todtransports.www.BackendDriver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendDriverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
